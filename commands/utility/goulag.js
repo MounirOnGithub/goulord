58dc599cfdfb2a38490bdbe956adbcba71b8d03e
Mounir KHANOURI <mkhanouri555@gmail.com>
2025-06-21 14:05:28 +0200

    fix: could not move multiple users with their own timeout

--- commands/utility/goulag.js
+++ commands/utility/goulag.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 require('dotenv').config()
 
+const activeGoulagTimers = new Map()
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('goulag')
@@ -16,6 +18,14 @@ module.exports = {
         const member = interaction.guild.members.cache.get(user.id)
         const goulagChannelId = process.env.GOULAG_CHANNEL_ID
 
+        if (activeGoulagTimers.has(user.id)) {
+            return interaction.reply({ content: 'Il est déjà au goulag tqt', ephemeral: true })
+        }
+
+        if (!member.voice.channel) {
+            return interaction.reply({ content: 'Il est pas connecté ce fou, donc ça marchera pas', ephemeral: true })
+        }
+
         try {
             const originalRoles = member.roles.cache.map(role => role.id);
             const originalChannelId = member.voice.channelId;
@@ -25,12 +35,19 @@ module.exports = {
             await interaction.reply(`${user.username} a été envoyé au Goulag !`)
 
             // Attendre 3 minutes
-            setTimeout(async () => {
-                // Revenir à son canal d'origine ou à un autre traitement
-                await member.roles.set(originalRoles);
-                await member.voice.setChannel(originalChannelId ? originalChannelId : null);
-                await interaction.followUp(`${user.username} est sorti du goulag.`);
+            const timeout = setTimeout(async () => {
+                try {
+                    // Revenir à son canal d'origine ou à un autre traitement
+                    await member.roles.set(originalRoles);
+                    await member.voice.setChannel(originalChannelId ? originalChannelId : null);
+                    await interaction.followUp(`${user.username} est sorti du goulag.`);
+                } catch (error) {
+                    interaction.reply(`Un problème est survenu pour faire revenir ${user.username}`)
+                    console.error(error)
+                }
             }, 3 * 60 * 1000); // 3 minutes en millisecondes
+
+            activeGoulagTimers.set(user.id, timeout)
         } catch (error) {
             console.error('Erreur lors du déplacement du membre:', error);
             await interaction.reply('Une erreur est survenue lors du déplacement du membre.');
